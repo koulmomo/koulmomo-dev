0b794c18fff8f0842074f092812de87ddee7b6ac
Mo Kouli <mkouli@uber.com>
2020-05-19 22:57:40 -0700

    💄 dark mode support

--- src/components/app/material-ui-theme.component.tsx
+++ src/components/app/material-ui-theme.component.tsx
@@ -1,16 +1,34 @@
 import * as React from "react";
 
-import { createMuiTheme } from "@material-ui/core/styles";
-import { ThemeProvider } from "@material-ui/core/styles";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import CssBaseline from "@material-ui/core/CssBaseline";
-
-export const theme = createMuiTheme();
 
 export interface Props {
   children: JSX.Element[] | JSX.Element;
 }
 
 export default function MaterialUITheme(props: Props) {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = React.useMemo(() => {
+    const palette = prefersDarkMode
+      ? {
+          primary: { main: "#b39ddb" },
+          secondary: { main: "#ff4081" },
+        }
+      : {
+          primary: { main: "#673AB7" },
+          secondary: { main: "#F50057" },
+        };
+
+    return createMuiTheme({
+      palette: {
+        ...palette,
+        type: prefersDarkMode ? "dark" : "light",
+      },
+    });
+  }, [prefersDarkMode]);
+
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
